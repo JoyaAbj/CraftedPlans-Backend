61404b9bc9acc5e92f727c7fa7c5f162e2298cdf
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2024-01-12 15:38:20 +0200

    getall users-login-register-deleteuser-updateuser

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,6 +36,70 @@ const register = async (req, res) => {
     }
 };
 
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) throw Error("All fields must be filled");
+        const exist = await Users.findOne({ email });
+        if (!exist) throw Error("Not registered yet");
+        const comparing = await bcrypt.compare(password, exist.password);
+        if(!comparing)throw Error("Passwords does not match");
+        const token = generateToken(exist._id, exist.role);
+        res.status(200).json({ message: "login successfully", token, id:exist._id });
+    } catch (error) {
+        res.status(500).json({ message: `Failed to login by ${email}`, error: error.message })
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const { Id } = req.params;
+    try {
+        if(!Id)throw Error("No id passed as parameter");
+        const resultat = await Users.findByIdAndDelete({ _id:Id });
+        if (!resultat) throw Error("An error occured");
+        const users =await Users.find({});
+        res.status(200).json({ message: "One of users deleted successfully", users});
+    } catch (error) {
+        res.status(500).json({ message: "An error occured during deleting a user", error: error.message })
+    }
+};
+
+const getAll = async (req, res) => {
+    try {
+        const users = await Users.find({});
+        res.status(200).json({ message: "Selecing all users successfully", users });
+    } catch (error) {
+        res.status(500).json({ message: 'An error occured during selecting all users', error: error.message })
+    }
+};
+
+const updateUser = async (req, res) => {
+    const { fullName, phoneNumber, email } = req.body;
+    const { Id } = req.params
+    try {
+        if (!fullName || !phoneNumber || !email) throw Error('All fields must be filled');
+        if (!Id) throw Error("No id sent as parameter");
+        const resultat = await Users.findByIdAndUpdate({ _id:Id }, { fullName, phoneNumber, email });
+        if(!resultat)throw Error("Error while updating");
+        const user=await getUserById(Id);
+        res.status(200).json({ message: "Updating a user successfully" ,user});
+    } catch (error) {
+        res.status(500).json({ message: "Failed to update a user", error: error.message })
+    }
+};
+
+const getUserById = async(Id)=>{
+    try {
+      const user= await Users.findById({_id:Id});
+      return user;
+    } catch (error) {
+      return error;
+    }
+  }
 module.exports = {
      register,
+     login,
+     deleteUser,
+     getAll,
+     updateUser,
 };
